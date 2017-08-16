b7c5ab5403de439ef9aa13b5594ee6ce1598d6b1
k-petite <katefanchic@mail.ru>
2017-08-16 23:35:53 +0300

    Adjusted highlights

--- js/app.js
+++ js/app.js
@@ -55,6 +55,19 @@ function setCurTime16() {
 // Attach a timeupdate event to the video element, and execute a function if the current playback position has changed
 vid.addEventListener("timeupdate", myFunction);
 
+function textBlack(){
+  var list = document.getElementsByClassName("textWrapper")[0];
+    for(i=0; i<16; i+=1){
+    list.getElementsByTagName('button')[i].style.color='black';
+    list.getElementsByTagName('button')[i].style.background='none';
+    }
+}
+
+function textHighlight(number, color){
+  var list = document.getElementsByClassName("textWrapper")[0];
+    list.getElementsByTagName('button')[number].style.background=color;
+}
+
 function myFunction() {
     // Display the current position of the video in a p element with id="demo"
     var list = document.getElementsByClassName("textWrapper")[0];
@@ -62,49 +75,81 @@ function myFunction() {
     let time = vid.currentTime;
 
     if (time>0 & time<3.8) {
-      list.getElementsByTagName('button')[0].style.background='rgb(176, 246, 234)';
+      textHighlight('0', 'rgb(176, 246, 234)');
     }
 
     if (time>3.8 & time<8){
-      for(i=0; i<16; i+=1){
-      list.getElementsByTagName('button')[i].style.color='black';
-      list.getElementsByTagName('button')[i].style.background='none';
-      list.getElementsByTagName('button')[1].style.background='rgb(176, 246, 224)';
-      }
+      textBlack();
+      textHighlight('1', 'rgb(176, 246, 214)');
     }
 
     if (time>8 & time<11){
-      for(i=0; i<16; i+=1){
-        list.getElementsByTagName('button')[i].style.color='black';
-        list.getElementsByTagName('button')[i].style.background='none';
-        list.getElementsByTagName('button')[2].style.background='rgb(176, 246, 214)';
-      }
+      textBlack();
+      textHighlight('2', 'rgb(176, 246, 194)');
     }
 
     if (time>11 & time<13){
-      for(i=0; i<16; i+=1){
-        list.getElementsByTagName('button')[i].style.color='black';
-        list.getElementsByTagName('button')[i].style.background='none';
-        list.getElementsByTagName('button')[3].style.background='rgb(176, 246, 204)';
-      }
+      textBlack();
+      textHighlight('3', 'rgb(176, 246, 174)');
     }
 
     if (time>13 & time<17){
-      for(i=0; i<16; i+=1){
-        list.getElementsByTagName('button')[i].style.color='black';
-        list.getElementsByTagName('button')[i].style.background='none';
-        list.getElementsByTagName('button')[4].style.background='rgb(176, 246, 194)';
-      }
+      textBlack();
+      textHighlight('4', 'rgb(176, 246, 154)');
     }
 
     if (time>17 & time<22.370){
-      for(i=0; i<16; i+=1){
-        list.getElementsByTagName('button')[i].style.color='black';
-        list.getElementsByTagName('button')[i].style.background='none';
-        list.getElementsByTagName('button')[5].style.background='rgb(176, 246, 184)';
-      }
+      textBlack();
+      textHighlight('5', 'rgb(176, 246, 134)');
     }
 
+    if (time>22.370 & time<26.880){
+      textBlack();
+      textHighlight('6', 'rgb(176, 246, 114)');
+    }
 
+    if (time>26.880 & time<32.100){
+      textBlack();
+      textHighlight('7', 'rgb(176, 246, 94)');
+    }
 
+    if (time>32.100 & time<34.730){
+      textBlack();
+      textHighlight('8', 'rgb(176, 246, 74)');
+    }
+
+    if (time>34.730 & time<39.430){
+      textBlack();
+      textHighlight('9', 'rgb(176, 246, 54)');
+    }
+
+    if (time>39.430 & time<42.350){
+      textBlack();
+      textHighlight('10', 'rgb(176, 246, 34)');
+    }
+
+    if (time>42.350 & time<46.310){
+      textBlack();
+      textHighlight('11', 'rgb(176, 246, 14)');
+    }
+
+    if (time>46.310 & time<49.270){
+      textBlack();
+      textHighlight('12', 'rgb(176, 226, 0)');
+    }
+
+    if (time>49.270 & time<53.760){
+      textBlack();
+      textHighlight('13', 'rgb(176, 206, 0)');
+    }
+
+    if (time>53.760 & time<57.650){
+      textBlack();
+      textHighlight('14', 'rgb(176, 186, 0)');
+    }
+
+    if (time>57.650){
+      textBlack();
+      textHighlight('15', 'rgb(176, 166, 0)');
+    }
 }
