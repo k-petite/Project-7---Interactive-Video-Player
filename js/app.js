d512906a94b079d9cebba8e9d1953ed53f0ffbb3
k-petite <katefanchic@mail.ru>
2017-09-03 21:59:19 +0300

    Updated comments

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 var vid = document.getElementById("video-player");
 console.log(vid.currentTime);
 
+//Move video according to the text clicked
 function setCurTime1() {
     vid.currentTime=0.240;
 }
@@ -50,9 +51,9 @@ function setCurTime16() {
     vid.currentTime=57.650;
 }
 
-// Get the video element with id="myVideo"
 
-// Attach a timeupdate event to the video element, and execute a function if the current playback position has changed
+
+//Highlight text when played
 vid.addEventListener("timeupdate", myFunction);
 
 function textBlack(){
@@ -69,82 +70,80 @@ function textHighlight(number, color){
 }
 
 function myFunction() {
-    // Display the current position of the video in a p element with id="demo"
-    var list = document.getElementsByClassName("textWrapper")[0];
 
     let time = vid.currentTime;
 
-    if (time>0 & time<3.8) {
+    if (time>0 && time<3.8) {
       textBlack();
       textHighlight('0', 'rgb(176, 246, 234)');
     }
 
-    if (time>3.8 & time<8){
+    if (time>3.8 && time<8){
       textBlack();
       textHighlight('1', 'rgb(176, 246, 214)');
     }
 
-    if (time>8 & time<11){
+    if (time>8 && time<11){
       textBlack();
       textHighlight('2', 'rgb(176, 246, 194)');
     }
 
-    if (time>11 & time<13){
+    if (time>11 && time<13){
       textBlack();
       textHighlight('3', 'rgb(176, 246, 174)');
     }
 
-    if (time>13 & time<17){
+    if (time>13 && time<17){
       textBlack();
       textHighlight('4', 'rgb(176, 246, 154)');
     }
 
-    if (time>17 & time<22.370){
+    if (time>17 && time<22.370){
       textBlack();
       textHighlight('5', 'rgb(176, 246, 134)');
     }
 
-    if (time>22.370 & time<26.880){
+    if (time>22.370 && time<26.880){
       textBlack();
       textHighlight('6', 'rgb(176, 246, 114)');
     }
 
-    if (time>26.880 & time<32.100){
+    if (time>26.880 && time<32.100){
       textBlack();
       textHighlight('7', 'rgb(176, 246, 94)');
     }
 
-    if (time>32.100 & time<34.730){
+    if (time>32.100 && time<34.730){
       textBlack();
       textHighlight('8', 'rgb(176, 246, 74)');
     }
 
-    if (time>34.730 & time<39.430){
+    if (time>34.730 && time<39.430){
       textBlack();
       textHighlight('9', 'rgb(176, 246, 54)');
     }
 
-    if (time>39.430 & time<42.350){
+    if (time>39.430 && time<42.350){
       textBlack();
       textHighlight('10', 'rgb(176, 246, 34)');
     }
 
-    if (time>42.350 & time<46.310){
+    if (time>42.350 && time<46.310){
       textBlack();
       textHighlight('11', 'rgb(176, 246, 14)');
     }
 
-    if (time>46.310 & time<49.270){
+    if (time>46.310 && time<49.270){
       textBlack();
       textHighlight('12', 'rgb(176, 226, 0)');
     }
 
-    if (time>49.270 & time<53.760){
+    if (time>49.270 && time<53.760){
       textBlack();
       textHighlight('13', 'rgb(176, 206, 0)');
     }
 
-    if (time>53.760 & time<57.650){
+    if (time>53.760 && time<57.650){
       textBlack();
       textHighlight('14', 'rgb(176, 186, 0)');
     }
@@ -155,6 +154,8 @@ function myFunction() {
     }
 }
 
+
+// Styling controls bar
 var controls = document.getElementsByClassName("mejs__controls");
 
 controls[0].style.background='rgba(121, 212, 191, 0.2)';
